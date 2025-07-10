2fe7b906fe39593580c41156b58d5840e555a4fa
烨 潘 <19852336187@163.com>
2025-07-10 23:01:33 +0800

    7月10号作业更新

--- 3.c
+++ 3.c
@@ -220,51 +220,165 @@
 //    }
 //
 //    return 0;
+////}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <windows.h>  // 用于Sleep()函数和system()函数
+//
+//int main() {
+//    int minutes;
+//    int seconds;
+//
+//    printf("电脑倒计时关机程序\n");
+//    printf("请输入倒计时时间（分钟）：");
+//    if (scanf("%d", &minutes) != 1) { // 检查输入是否为整数
+//        printf("输入无效，请输入一个整数！\n");
+//        system("pause");
+//        return 1;
+//    }
+//
+//    if (minutes <= 0) {
+//        printf("时间必须大于0分钟！\n");
+//        system("pause");
+//        return 1;
+//    }
+//
+//    seconds = minutes * 60;
+//
+//    printf("倒计时开始，将在%d分钟后关机...\n", minutes);
+//    printf("按Ctrl+C可以取消关机\n");
+//
+//    // 设置关机命令（/t参数指定倒计时时间，单位秒）
+//    char shutdownCmd[100];
+//    sprintf(shutdownCmd, "shutdown -s -t %d", seconds);
+//    system(shutdownCmd);
+//
+//    return 0;
 //}
-#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>  // 用于Sleep()函数和system()函数
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c, c++, ++a, a++;
+//	b += a++ + c;
+//	printf("a = %d b = %d c = %d\n:", a, b, c);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//
+//int main() {
+//    int a = 343, b = 156;
+//
+//    printf("Before swap: a = %d, b = %d\n", a, b);
+//
+//    a = a ^ b;
+//    b = a ^ b;
+//    a = a ^ b;
+//
+//    printf("After swap: a = %d, b = %d\n", a, b);
+//
+//    return 0;
+//}
 
-int main() {
-    int minutes;
-    int seconds;
+//#include <stdio.h>
+//
+//int countOnes(int n) {
+//    int count = 0;
+//    for (int i = 0; i < 32; i++) {
+//        if (n & (1 << i)) {  // 检查第 i 位是否为 1
+//            count++;
+//        }
+//    }
+//    return count;
+//}
+//
+//int main() {
+//    int n;
+//    scanf("%d", &n);
+//    printf("%d\n", countOnes(n));
+//    return 0;
+//}
+//#include <stdio.h>
+//int main() {
+//    int n;
+//    while (scanf("%d", &n) != EOF) {
+//        int count = 0;
+//        for (int i = 0; i < 32; i++) {
+//            if (n & (1 << i)) {
+//                count++;
+//            }
+//        }
+//        printf("%d\n", count);
+//    }
+//    return 0;
+//}
 
-    printf("电脑倒计时关机程序\n");
-    printf("请输入倒计时时间（分钟）：");
-    if (scanf("%d", &minutes) != 1) { // 检查输入是否为整数
-        printf("输入无效，请输入一个整数！\n");
-        system("pause");
-        return 1;
-    }
-
-    if (minutes <= 0) {
-        printf("时间必须大于0分钟！\n");
-        system("pause");
-        return 1;
-    }
-
-    seconds = minutes * 60;
-
-    printf("倒计时开始，将在%d分钟后关机...\n", minutes);
-    printf("按Ctrl+C可以取消关机\n");
-
-    // 设置关机命令（/t参数指定倒计时时间，单位秒）
-    char shutdownCmd[100];
-    sprintf(shutdownCmd, "shutdown -s -t %d", seconds);
-    system(shutdownCmd);
-
-    // 显示倒计时
-    while (seconds > 0) {
-        int remainingMinutes = seconds / 60;
-        int remainingSeconds = seconds % 60;
-
-        printf("\r剩余时间: %02d:%02d", remainingMinutes, remainingSeconds);
-        fflush(stdout);  // 刷新输出缓冲区
-
-        Sleep(1000);  // 等待1秒
-        seconds--;
-    }
-
-    printf("\n时间到，正在关机...\n");
-    return 0;
-}
+//
+//#include <stdio.h>
+//
+//void printBinaryDigits(int num) {
+//    printf("二进制表示: ");
+//    // 打印完整32位二进制
+//    for (int i = 31; i >= 0; i--) {
+//        printf("%d", (num >> i) & 1);
+//    }
+//    printf("\n");
+//
+//    printf("偶数位(0、2、4...30位): ");
+//    for (int i = 30; i >= 0; i -= 2) {  // 从最高偶数位开始
+//        printf("%d", (num >> i) & 1);
+//    }
+//    printf("\n");
+//
+//    printf("奇数位(1、3、5...31位): ");
+//    for (int i = 31; i >= 1; i -= 2) {  // 从最高奇数位开始
+//        printf("%d", (num >> i) & 1);
+//    }
+//    printf("\n");
+//}
+//
+//int main() {
+//    int num;
+//    printf("请输入一个整数: ");
+//    scanf("%d", &num);
+//
+//    printBinaryDigits(num);
+//
+//    return 0;
+////}
+//#include <stdio.h>
+//
+//int main() {
+//    int a, b;
+//    while (scanf("%d %d", &a, &b) != EOF) {
+//        int count = 0;
+//        int i = 0;
+//        for (i = 0; i < 32; i++) {
+//            if (((a >> i) & 1) == ((b >> i) & 1)) {
+//                count++;
+//            }
+//        }
+//        printf("%d\n", count);
+//    }
+//    return 0;
+////}
+//
+//#include <stdio.h>
+//
+//int count(int n) {
+//	if (n == 1) return 1;
+//	else if (n == 2) return 2;
+//	else return (count(n - 1) + count(n - 2));
+//	
+//}
+//int main() {
+//	int n;
+//	while (scanf("%d", &n) != EOF) {
+//		printf("%d\n", count(n));
+//	}
+//	return 0;
+//}
