54c2ade7530279668c702f4c237f2d272e6524c6
烨 潘 <19852336187@163.com>
2025-07-13 00:31:15 +0800

    7月13日博客的一些代码

--- 4.c
+++ 4.c
@@ -124,13 +124,14 @@
 //}
 
 
-//
+
 //#include <stdio.h>	
 //
 //int main() {
 //	int n;
 //	printf("请输入一个层数： ");
 //	scanf("%d", &n);
+//	printf("\n");	
 //	int i, j, k;
 //	for (i = 0; i < n; i++) {
 //		if (i <=(n / 2)) {
@@ -141,7 +142,6 @@
 //				printf("*");
 //			}
 //			printf("\n");
-//			continue;
 //		}else {
 //			for (j =(i-(n/2)) ; j > 0; j--) {
 //				printf(" ");
@@ -150,48 +150,128 @@
 //				printf("*");
 //			}
 //			printf("\n");
-//			continue;
 //		}
-//
-//		return 0;
 //	}
+//	return 0;
+//}
+////#include <stdio.h>
+////
+////void printDiamond(int n) {
+////    int i, j, space;
+////    for (i = 1; i <= n; i++) {
+////        for (space = 1; space <= n - i; space++) {
+////            printf(" ");
+////        }      
+////        for (j = 1; j <= 2 * i - 1; j++) {
+////            printf("*");
+////        }
+////        printf("\n");
+////    }
+////    for (i = n - 1; i >= 1; i--) {
+////        for (space = 1; space <= n - i; space++) {
+////            printf(" ");
+////        }
+////        for (j = 1; j <= 2 * i - 1; j++) {
+////            printf("*");
+////        }
+////        printf("\n");
+////    }
+////}
+//////
+////int main() {
+////    int rows;
+////
+////    printf("请输入菱形的行数: ");
+////    scanf("%d", &rows);
+////
+////    if (rows % 2 == 0) {
+////        printf("输入应为奇数，已自动加1\n");
+////        rows++;
+////    }
+////
+////    printDiamond((rows + 1) / 2);
+////
+////    return 0;
+////}
+//#include <stdio.h>
+//#include <stdlib.h> // 包含abs函数
+//
+//int main() {
+//    int n;
+//    do {
+//        printf("请输入菱形的层数(奇数): ");
+//        scanf("%d", &n);
+//    } while (n % 2 == 0);
+//
+//    int center = n / 2; // 中心坐标
+//    
+//    // 使用笛卡尔坐标系思路
+//    for (int y = -center; y <= center; y++) {
+//        for (int x = -center; x <= center; x++) {
+//            // 菱形边界条件：|x| + |y| <= center
+//            if (abs(x) + abs(y) <= center) {
+//                printf("*");
+//            } else {
+//                printf(" ");
+//            }
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+////}
+//#include <stdio.h>
+//
+//int main() {
+//    int n;
+//    do {
+//        printf("请输入菱形的层数(奇数): ");
+//        scanf("%d", &n);
+//    } while (n % 2 == 0); // 确保输入为奇数
+//
+//    int mid = n / 2; // 中心点索引
+//
+//    // 打印上半部分（含中心）
+//    for (int i = 0; i <= mid; i++) {
+//        // 打印空格：每行空格数 = 中心索引 - 当前行索引
+//        for (int j = 0; j < mid - i; j++)
+//            printf(" ");
+//
+//        // 打印星号：每行星号数 = 当前行索引*2 + 1
+//        for (int j = 0; j < 2 * i + 1; j++)
+//            printf("*");
+//        printf("\n");
+//    }
+//
+//    // 打印下半部分
+//    for (int i = mid - 1; i >= 0; i--) {
+//        for (int j = 0; j < mid - i; j++)
+//            printf(" ");
+//        for (int j = 0; j < 2 * i + 1; j++)
+//            printf("*");
+//        printf("\n");
+//    }
+//    return 0;
 //}
 //#include <stdio.h>
 //
-//void printDiamond(int n) {
-//    int i, j, space;
-//    for (i = 1; i <= n; i++) {
-//        for (space = 1; space <= n - i; space++) {
-//            printf(" ");
-//        }      
-//        for (j = 1; j <= 2 * i - 1; j++) {
-//            printf("*");
-//        }
-//        printf("\n");
-//    }
-//    for (i = n - 1; i >= 1; i--) {
-//        for (space = 1; space <= n - i; space++) {
-//            printf(" ");
-//        }
-//        for (j = 1; j <= 2 * i - 1; j++) {
-//            printf("*");
-//        }
-//        printf("\n");
-//    }
-//}
-//
 //int main() {
-//    int rows;
+//    int n;
+//    do {
+//        printf("请输入菱形的层数(奇数): ");
+//        scanf("%d", &n);
+//    } while (n % 2 == 0);//中心点索引
 //
-//    printf("请输入菱形的行数: ");
-//    scanf("%d", &rows);
+//    int mid = n / 2;
 //
-//    if (rows % 2 == 0) {
-//        printf("输入应为奇数，已自动加1\n");
-//        rows++;
+//    for (int i = -mid; i <= mid; i++) {
+//        int spaces = (i > 0) ? i : -i; // 计算当前行与中心点的距离
+//        int stars = n - 2 * spaces;//计算当前行星号数
+//
+//        for (int j = 0; j < spaces; j++)
+//            printf(" ");
+//        for (int j = 0; j < stars; j++)
+//            printf("*");
+//        printf("\n");
 //    }
-//
-//    printDiamond((rows + 1) / 2);
-//
 //    return 0;
 //}
